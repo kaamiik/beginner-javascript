04452e060efc39e0ed7af7f4b8de7597539e2d8b
Kamran Kiani <kiani.km@gmail.com>
2025-01-09 14:32:38 +0330

    Arrays section

--- playground/hoisting-FINISHED.js
+++ playground/hoisting-FINISHED.js
@@ -6,11 +6,20 @@ age = 10;
 /* What does this file do? */
 sayHi();
 
+
+
+
+
+
+
+
+
 /* How does this file do it? */
 function sayHi() {
   console.log('hey!');
   console.log(add(10, 2));
 }
+
 
 function add(a, b) {
   return a + b;
